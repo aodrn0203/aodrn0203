97f1a6a73d9eb0d6768fe41e497d53d1e7665690
aodrn0203 <141035183+aodrn0203@users.noreply.github.com>
2023-12-11 20:16:14 +0900

    [Silver II] Title: N번째 큰 수, Time: 556 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,17 +1,2 @@
-#include<queue>
-#include<stdio.h>
-using namespace std;
-int main()
-{
-	priority_queue <int> pq;
-	int N,k;
-	scanf("%d",&N);
-    for (int i = 0; i < N * N; i++)
-	{
-        scanf("%d",&k);
-		pq.push(k*-1);
-        if (pq.size() > N) pq.pop();
-    }
-	
-	printf("%d",-1*pq.top());
-}
+#include<bits/stdc++.h>
+main(){std::priority_queue<int>pq;int i,N,k;scanf("%d",&N);for(i=0;i<N*N;i++){scanf("%d",&k);pq.push(k*-1);if(pq.size()>N)pq.pop();}printf("%d",-1*pq.top());}
